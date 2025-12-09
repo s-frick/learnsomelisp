f41b2e5c07e4abf6c8e57a4d9af679f5af9dd2f2
Sebastian Frick <sebastian@monkeysquad.de>
2025-12-09 12:43:29 +0100

    practical commonlisp 03

---
+++ practical_commonlisp/03_simple_db/01_lists.lisp
@@ -0,0 +1,85 @@
+;; property list with keyword symbols
+(list :a 1 :b 2 :c 3)
+
+;; get props from list by name
+(getf (list :a 1 :b 2 :c 3) :c)
+
+
+;;; DATABASE
+(defun make-cd (title artist rating ripped)
+  (list :title title :artist artist :rating rating :ripped ripped))
+
+;; global variable *db*
+(defvar *db* nil)
+
+(defun add-record (cd) (push cd *db*))
+(defun dump-db ()
+  (dolist (cd *db*)
+    (format t "~{~a:~10t~a~%~}~%" cd)))
+
+;; prompt
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+    (prompt-read "Title")
+    (prompt-read "Artist")
+    (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+    (y-or-n-p "Ripped [y/n]: ")
+  ))
+
+(defun add-cds ()
+  (loop (add-record (prompt-for-cd))
+        (if (not (y-or-n-p "Another? [y/n]: ")) (return))))
+
+(defun save-db (filename)
+  (with-open-file (out filename
+                       :direction :output
+                       :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
+
+(defun select-by-artist (artist) 
+  (remove-if-not 
+    #'(lambda (cd) (equal (getf cd :artist) artist))
+  *db*))
+
+(defun select(selector-fn)
+    (remove-if-not selector-fn *db*))
+
+(defun artist-selector (artist)
+  #'(lambda (cd) (equal (getf cd :artist) artist)))
+
+;; keyword parameters - bind call args to corresponding values
+(defun foo (&key a b c) (list a b c))
+(foo :a 1 :c 3) ;; ==> (1 NIL 3) | NIL is bound to b, because no value was given for b
+
+(defun foo (&key a (b 20) (c 30 c-p)) (list a b c c-p))
+(foo :a 1 :c 3) ;; ==> (1 20 3 T) | now b is 20 (default value)
+(foo) ;; ==> (1 20 30 NIL) | now b is 20 (default value), c is default to 30 and c-p (supplied-p parameter) is nil=false
+
+
+;;;; Operate on DB
+
+;; fill in some cds
+; (add-record (make-cd "Roses" "Kathy Mattea" 7 t))
+; (add-record (make-cd "Fly" "Dixie Chicks" 8 t))
+; (add-record (make-cd "Home" "Dixie Chicks" 9 t))
+
+;; dump the db
+; (dump-db)
+
+
+
+
+;;;; Main
+; (add-cds)
+(save-db "./cds.db")
